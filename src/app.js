a792d4937f4700b81c940cda19f5214c6a56e6b9
sspantz <sspantiezheng@gmail.com>
2021-07-30 17:11:51 +0800

    config babel

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-console */
-const express = require('express');
+const express = require("express")
 
-const app = express();
+const app = express()
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    status: 'OK',
-  });
-});
+    status: "OK",
+  })
+})
 
-const port = 4000;
-app.listen(port, () => console.log(`listening on port ${port}.`));
+const port = 4000
+
+exports.start = () =>
+  app.listen(port, () => console.log(`listening on port ${port}.`))
