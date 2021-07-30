a9d5abee0bd3c3cb58a72526bc7c4e2842e93415
sspantz <sspantiezheng@gmail.com>
2021-07-30 15:26:03 +0800

    install express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "OK",
+  });
+});
+
+const port = 4000;
+app.listen(port, () => console.log(`listening on port ${port}.`));
